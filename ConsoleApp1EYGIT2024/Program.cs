9fff5ab311b08326d7e2198fb59a608372335726
HarmeshKr <harmesh7878@gmail.com>
2024-10-06 14:27:22 +0530

    First Commit

---
+++ ConsoleApp1EYGIT2024/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1EYGIT2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
